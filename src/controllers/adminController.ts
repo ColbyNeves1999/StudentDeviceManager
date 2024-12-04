e67393ef556841364c099c4b42fee8a58043732f
ColbyNeves1999 <colby.neves@smail.astate.edu>
2024-12-04 15:47:10 -0600

    Notes added

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -10,11 +10,13 @@ async function initializeAdmins(req: Request, res: Response): Promise<void> {
 
     //Grabs email for the primary admin user
     const user_exists = await getUserByEmail(ADMIN_EMAIL);
-  
+
+    //Verifies that a user with that email doesn't already exist
     if (!user_exists) {
         await addUser(ADMIN_EMAIL, await argon2.hash(ADMIN_PASS));
     }
 
+    //Redirects to primary drop page
     res.redirect('/index');
 
 }
